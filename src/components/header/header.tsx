5cd4f59b0ecd37b97c61b1afd29fd8e49054e273
isaaconek <onekdev5@gmail.com>
2024-11-18 11:22:57 +0100

    Fixation de la navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ import Onglets from "./components/onglets"
 
 const Header = () => {
     return (
-        <nav className="flex justify-between items-center ~p-2/4 ~pl-5/12 ~pr-5/12 relative top-[-20rem] shadow-md ">
+        <nav className="flex justify-between items-center ~p-2/4 ~pl-5/12 ~pr-5/12 fixed top-[0.1rem] w-full z-20 shadow-md ">
            <Favicon/>
            <div className="hidden sm:flex items-center gap-7">
                 <Onglets/>
