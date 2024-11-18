b993460095a017cc1051f8644411e0b255cb3950
isaaconek <onekdev5@gmail.com>
2024-11-18 17:16:54 +0100

    Animation de la sidebar a son apparition

--- src/components/header/components/sideBar.tsx
+++ src/components/header/components/sideBar.tsx
@@ -3,6 +3,7 @@ import SideBarOnglet from './sideBarOnglet'
 import { RxCross1 } from "react-icons/rx";
 import UserAvatar from './userAvatar';
 import IconUser from './iconUser';
+import {motion} from 'framer-motion'
 
 interface SidebarProps {
     userName : string | undefined,
@@ -19,36 +20,50 @@ const tbOnglet: ItemOnglet[] = [
     {to: '/success-stories', text: 'Success-stories'},
 ]
 
+const sideBarVariant = {
+    initial : {
+        opacity: 0,
+        x: 20
+    },
+    animate : {
+        opacity: 1,
+        x: 1
+    }
+}
+
 const Sidebar = ({ userName, userAvatar, setSideBar }: SidebarProps) => {
     const hiddeSideBar = () => {
         setSideBar (false)
     }
-  return (
-    <div className="w-64 h-screen flex flex-col bg-white border-r border-gray-200 shadow-lg absolute top-0 left-36 p-3 ">
-        {/* Entete */}
-        <div className="p-4 border-b border-gray-200 flex items-center justify-between pr-6 ">
-            <img src="public/mosala.png" alt="logo CRD" className='w-8' />
-            <RxCross1 className='text-black text-xl' onClick={hiddeSideBar} />
-        </div>
-      
-        {/* Onglets */}
-        <nav className="flex-1 overflow-y-auto">
-            <ul className="flex flex-col gap-2">
-                {
-                    tbOnglet.map ((onglet) => (
-                        <SideBarOnglet to={onglet.to} text={onglet.text}/>
-                    ))
-                }
-            </ul>
-        </nav>
+    return (
+        <motion.div 
+            className="w-64 h-screen flex flex-col bg-white border-r border-gray-200 shadow-lg absolute top-0 left-36 p-3"
+            variants={sideBarVariant} initial="initial" animate="animate"
+        >
+            {/* Entete */}
+            <div className="p-4 border-b border-gray-200 flex items-center justify-between pr-6 ">
+                <img src="public/mosala.png" alt="logo CRD" className='w-8' />
+                <RxCross1 className='text-black text-xl' onClick={hiddeSideBar} />
+            </div>
+        
+            {/* Onglets */}
+            <nav className="flex-1 overflow-y-auto">
+                <ul className="flex flex-col gap-2">
+                    {
+                        tbOnglet.map ((onglet) => (
+                            <SideBarOnglet to={onglet.to} text={onglet.text}/>
+                        ))
+                    }
+                </ul>
+            </nav>
 
-        {/* Bouton */}
-        <button className="flex items-center bg-main-yellow rounded-full p-2 gap-2">
-            {userAvatar ? <UserAvatar userAvatar={userAvatar} userName={userName} /> : <IconUser/> }
-            <span className="text-sm font-medium">{userName? userName : 'Connexion | Inscription'}</span>
-        </button>
-    </div>
-  )
+            {/* Bouton */}
+            <button className="flex items-center bg-main-yellow rounded-full p-2 gap-2">
+                {userAvatar ? <UserAvatar userAvatar={userAvatar} userName={userName} /> : <IconUser/> }
+                <span className="text-sm font-medium">{userName? userName : 'Connexion | Inscription'}</span>
+            </button>
+        </motion.div>
+    )
 }
 
 export default Sidebar
