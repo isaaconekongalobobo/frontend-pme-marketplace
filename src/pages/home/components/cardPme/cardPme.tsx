ca1e4d9a46eef04ac3d2791bef67c9323d26dd5f
isaaconek <onekdev5@gmail.com>
2024-11-19 19:25:34 +0100

    Animation de l'image de la card au avec framer motion

--- src/pages/home/components/cardPme/cardPme.tsx
+++ src/pages/home/components/cardPme/cardPme.tsx
@@ -1,13 +1,31 @@
+import {motion} from 'framer-motion'
 interface pmeCardType {
     image: string | undefined,
     name: string | undefined,
     services: string[] | undefined
 }
 
+const imageVariant = {
+    initial : {
+        opacity: 0,
+        y: -20
+    },
+    animate: {
+        opacity: 1,
+        y: 0
+    },
+    hover: {
+        y: -10,
+    }
+}
+
 const CardPme = ({image, name, services} : pmeCardType) => {
     return (
         <div className=" flex flex-col items-center gap-2 ">
-            <img src={image} alt={`Profil de ${name}`} className="~w-3/4/5/6 rounded-xl " />
+            <motion.img 
+                src={image} alt={`Profil de ${name}`} className="~w-3/4/5/6 rounded-xl shadow-black shadow-sm " 
+                variants={imageVariant} initial="initial" animate="animate" whileHover="hover"
+            />
             <div className=" flex flex-col text-center ">
                 <h3 className="text-black font-medium text-xl "> {name} </h3>
                 <p className="text-black" > { `${services![1]} +${services?.length}` } </p>
