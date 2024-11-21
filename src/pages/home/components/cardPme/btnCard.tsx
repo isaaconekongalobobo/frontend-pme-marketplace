ad4b96f73402cbac3df52915232958a87687dba8
isaaconek <onekdev5@gmail.com>
2024-11-21 18:18:24 +0100

    Ajout d'animation a l'affichage et au survol avec framer-motion

--- src/pages/home/components/cardPme/btnCard.tsx
+++ src/pages/home/components/cardPme/btnCard.tsx
@@ -1,16 +1,38 @@
 import { CgPlayListAdd } from "react-icons/cg";
 import { Link } from "react-router-dom";
+import {motion} from 'framer-motion'
 
+const btnVariant = {
+    initial: {
+        scale: 0.5,
+        opacity: 0,
+    },
+    animate: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            delay: 1
+        }
+    },
+    hover: {
+        scale: 1.1,
+        y: -8,
+    }
+}
 
 const BtnCard = ({idPme}:{idPme: number}) => {
     return (
         <Link to={`/pme-details/${idPme}`} >
-            <CgPlayListAdd 
-                title="Voir les details de la PME"
-                className="text-white
-                bg-black-marroon size-14 p-2 pl-3 pt-3 rounded-full border-white border-4 
-                shadow-xl relative bottom-[6rem] left-[9rem] hover:border-main-yellow hover:text-main-yellow "
-             />
+            <motion.button variants={btnVariant} initial="initial" animate="animate" whileHover="hover"
+                className="relative bottom-[7rem] left-[10rem] rounded-full size-[5rem] flex justify-center
+                bg-black-marroon  border-white  hover:border-main-yellow border-4 shadow-xl "
+            >
+                <CgPlayListAdd 
+                    title="Voir les details de la PME"
+                    className="text-white size-14 hover:text-main-yellow relative top-[2px] left-[2px] "
+                />                
+            </motion.button>
+
         </Link>
     );
 }
