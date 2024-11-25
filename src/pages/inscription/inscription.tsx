a47166eb0170b495a6372b2995d4c21373595ab6
isaaconek <onekdev5@gmail.com>
2024-11-25 08:17:38 +0100

    Creation du composant step1 pour le premier formulaire d'inscription et ajout de l'image

--- src/pages/inscription/inscription.tsx
+++ src/pages/inscription/inscription.tsx
@@ -1,8 +1,8 @@
+import { Outlet } from "react-router-dom";
+
 const Inscription = () => {
     return (
-        <div>
-            
-        </div>
+        <Outlet></Outlet>
     );
 }
 
--- src/pages/inscription/step-1/step1.tsx
+++ src/pages/inscription/step-1/step1.tsx
@@ -1,8 +1,10 @@
 const Step1 = () => {
     return (
-        <div>
-            
-        </div>
+        <main className='flex justify-center items-center p-8 w-screen bg-breaked-white h-screen'>
+            <form className='bg-white p-9 pl-8 pr-8 shadow-lg rounded-2xl flex flex-col sm:flex-row gap-5' >
+                <img src="/img-step-1.jpg" className="w-60" />
+            </form>
+        </main>
     );
 }
 
