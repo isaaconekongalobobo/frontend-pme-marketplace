e1e812376bb29dabe8739cc126909cc9cf9b6256
isaaconek <onekdev5@gmail.com>
2024-11-29 09:48:50 +0100

    Ajout du style dans la section du composant Resultat categorie

--- src/pages/home/components/resultatCategorie.tsx
+++ src/pages/home/components/resultatCategorie.tsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 const ResultatCategorie = () => {
     const {nom} = useParams ()
     return (
-        <section>
+        <section className="grid sm:grid-cols-4 ~p-14/16 gap-14 mt-2 mb-28 ">
             <h1 className="text-black-marroon" > {nom} </h1>
         </section>
     );
