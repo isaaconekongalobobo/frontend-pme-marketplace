41d157afd57f0ae6b75326a372fbb8602fae0d55
isaaconek <onekdev5@gmail.com>
2024-11-29 10:34:01 +0100

    Ajout d'une ombre portee dans la barre de recherche

--- src/pages/home/components/searchBar.tsx
+++ src/pages/home/components/searchBar.tsx
@@ -13,7 +13,9 @@ const SearchBar = () => {
                 type="search" 
                 name="search" 
                 placeholder="Recherchez une PME | service | produit" 
-                className=" p-2 sm:~p-3/5 pl-4 w-3/4 sm:w-2/4 bg-white border-4 border-gray hover:border-black-marroon rounded-full text-black placeholder-black placeholder:~text-xs/sm "
+                className=" p-2 sm:~p-3/5 pl-4 w-3/4 sm:w-2/4 bg-white border-4 border-gray
+                hover:border-black-marroon rounded-full text-black placeholder-black 
+                placeholder:~text-xs/sm shadow-md "
             />
         </>
     );
