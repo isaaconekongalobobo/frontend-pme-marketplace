879feb0f2d45714fa3b0e805d16361e2433b971f
isaaconek <onekdev5@gmail.com>
2024-12-03 18:35:28 +0100

    Animation de la photo de profile

--- src/pages/pme-details/components/header/profileImage.tsx
+++ src/pages/pme-details/components/header/profileImage.tsx
@@ -1,7 +1,27 @@
+import {motion} from 'framer-motion'
+const imgVariant = {
+    initial: {
+        opacity: 0,
+        x:-20
+    },
+    animate: {
+        opacity: 1,
+        x:0 
+    },
+    hover: {
+        y: -10
+    },
+    tap: {
+        scale: 0.5
+    }
+}
 const ProfileImage = ({src}:{src: string | undefined}) => {
     return (
-        <img src={src} alt="Photo de profile" 
-        className=" size-28 sm:size-56 rounded-full shadow-xl border-8 border-baige " />
+        <motion.img src={src} alt="Photo de profile" 
+            className=" size-28 sm:size-56 rounded-full shadow-xl border-8 border-baige 
+            hover:border-main-yellow  " 
+            variants={imgVariant} initial="initial" animate="animate" whileHover="hover" whileTap="tap"
+        />
     );
 }
 
