152f0f9052623174387b79fc76ed461f1401a6ba
isaaconek <onekdev5@gmail.com>
2024-12-04 08:25:49 +0100

    Ajout d'une icone de fermeture pour le composant profileZoom; Animation du profile lors de son apparition

--- src/pages/pme-details/components/profileZoom.tsx
+++ src/pages/pme-details/components/profileZoom.tsx
@@ -1,9 +1,27 @@
+import { CgClose } from "react-icons/cg";
+import {motion} from 'framer-motion'
+
+const cardVariant = {
+    initial: {
+        opacity: 0,
+        scale: 0.5,
+    },
+    animate: {
+        opacity: 1,
+        scale: 1,
+    },
+}
+
 const ProfileZoom = ({src, showOrHiddeProfile}:{src:string | undefined, showOrHiddeProfile: () => void}) => {
     return (
         <div className="absolute flex justify-center w-full  p-10 z-10" onClick={showOrHiddeProfile}>
-            <div className="bg-black-marroon bg-opacity-75 pt-10">
-              <img src={src} alt="Photo de profil" className="w-96"  />  
-            </div>
+            <motion.div 
+                className="bg-black-marroon bg-opacity-75 pt-10 flex flex-col items-end"
+                variants={cardVariant} initial="initial" animate="animate"
+            >
+                <CgClose className="text-2xl hover:opacity-75 relative bottom-5 right-5" onClick={showOrHiddeProfile} />
+                <img src={src} alt="Photo de profil" className="w-96"  />  
+            </motion.div>
         </div>
     );
 }
