230a23d2b80332bc8c231c245016a2de4531ffb0
isaaconek <onekdev5@gmail.com>
2024-12-04 13:54:35 +0100

    Amelioration de la disposion des services

--- src/pages/pme-details/components/main/serviceSection/serviceSection.tsx
+++ src/pages/pme-details/components/main/serviceSection/serviceSection.tsx
@@ -40,11 +40,14 @@ const ServiceSection = ({name, idPme}:infos) => {
     
     
     return (
-        <section className="flex flex-col gap-1">
+        <section className="flex flex-col gap-3">
             <SectionTitle title="Services | Produits" />
-            <div>
-                <p className="text-black-marroon">{`${name?.toUpperCase()} vous propose plusieurs services et produits repondant a vos besoin.`}</p>
-                <p className="text-black-marroon">Voici nos services:</p>   
+            <div className="flex flex-col gap-3">
+                <div>
+                    <p className="text-black-marroon">{`${name?.toUpperCase()} vous propose plusieurs services et produits repondant a vos besoin.`}</p>
+                    <p className="text-black-marroon">Voici nos services:</p>    
+                </div>
+                   
                  {
                     loading ? <BiLoader className="text-black-marroon"/> : <ServiceList services={services} />
                  }           
