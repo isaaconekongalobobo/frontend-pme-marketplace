da913d70e92593130cf9871b546a7949650d0b68
isaaconek <onekdev5@gmail.com>
2024-12-05 11:19:58 +0100

    Animation du composant ImageItem ainsi que de son parent ServiceImages

--- src/pages/pme-details/components/main/serviceSection/imageItem.tsx
+++ src/pages/pme-details/components/main/serviceSection/imageItem.tsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
+import {motion} from 'framer-motion'
+
+const imageVariant = {
+    initial: { opacity: 0, x: -20 },
+    animate: { opacity: 1, x:0 },
+    hover: {y: -10}
+}
 
 const ImageItem = ({idService, image}:{idService: number, image: string}) => {
     return (
 
         <>
             <Link to={`/details/services/${idService}`}>
-                <div className="size-40 sm:size-72 bg-blue-600 rounded-xl bg-cover " style={{ backgroundImage: `url(${image})` }}></div>
+                <motion.div 
+                className="size-40 sm:size-72 bg-blue-600 rounded-xl bg-cover hover:opacity-5 " 
+                style={{ backgroundImage: `url(${image})` }}
+                variants={imageVariant} initial="initial" animate="animate" 
+                transition={{ duration: 0.5 }} whileHover="hover"
+                >
+                </motion.div>
             </Link>
         </>
     );
--- src/pages/pme-details/components/main/serviceSection/serviceImages.tsx
+++ src/pages/pme-details/components/main/serviceSection/serviceImages.tsx
@@ -1,13 +1,23 @@
 import { ServiceType } from "./serviceSection";
 import ImageItem from "./imageItem";
+import {motion} from 'framer-motion'
+const DivVariant = {
+    initial: { opacity: 0 },
+    animate: { 
+        opacity: 1, 
+        // Decalage de 0.1 seconde entre chaque enfant
+        transition: { staggerChildren: 0.1 } 
+    } 
+  }
 
 const ServiceImages = ({services}:{services: ServiceType[]}) => {
     return (
-    <div className=" grid grid-cols-2 sm:flex sm:flex-wrap gap-4 mt-5  ">
+    <motion.div className=" grid grid-cols-2 sm:flex sm:flex-wrap gap-4 mt-5" 
+    variants={DivVariant} initial="initial" animate="animate" >
         {
             services.map ((sv) => <ImageItem idService={sv.idService} image={sv.image}/> )
         }
-    </div>
+    </motion.div>
     );
 }
 
