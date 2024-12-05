a59d6d30900a715fc7a392ed93480e86b03ef4d7
isaaconek <onekdev5@gmail.com>
2024-12-05 11:36:47 +0100

    Creation d'un composant commandBtn qui appariat au survol de l'image d'un service; Animation de ce bouton

---
+++ src/pages/pme-details/components/main/serviceSection/commandBtn.tsx
@@ -0,0 +1,22 @@
+import {motion} from 'framer-motion'
+const pVariant = {
+    initial: {
+        opacity: 0,
+        y: 20
+    },
+    animate: {
+        opacity: 1,
+        y: 0
+    }
+}
+const CommandBtn = () => {
+    return <motion.p 
+            className="text-center text-xl font-medium bg-breaked-white
+             text-black-marroon p-2 rounded-full"
+            variants={pVariant} initial="initial" animate="animate"
+            >
+                Commander
+            </motion.p>
+}
+
+export default CommandBtn;
--- src/pages/pme-details/components/main/serviceSection/imageItem.tsx
+++ src/pages/pme-details/components/main/serviceSection/imageItem.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import {motion} from 'framer-motion'
+import { useState } from "react";
+import CommandBtn from "./commandBtn";
 
 const imageVariant = {
     initial: { opacity: 0, x: -20 },
@@ -8,20 +10,28 @@ const imageVariant = {
 }
 
 const ImageItem = ({idService, image}:{idService: number, image: string}) => {
+    const [link, setLink] = useState (false)
     return (
 
         <>
             <Link to={`/details/services/${idService}`}>
                 <motion.div 
-                className="size-40 sm:size-72 bg-blue-600 rounded-xl bg-cover hover:opacity-5 " 
+                className="size-40 sm:size-72 bg-blue-600 rounded-xl bg-cover hover:opacity-5 
+                flex items-center justify-center " 
                 style={{ backgroundImage: `url(${image})` }}
                 variants={imageVariant} initial="initial" animate="animate" 
                 transition={{ duration: 0.5 }} whileHover="hover"
+                onMouseOver={() => setLink (true)}
+                onMouseLeave={() => setLink (false)}
                 >
+                {
+                    link && <CommandBtn/>
+                }
                 </motion.div>
             </Link>
         </>
     );
 }
+
 
 export default ImageItem;
