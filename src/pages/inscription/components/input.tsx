e007d80d7238e97de0c4a442151701066817e267
isaaconek <onekdev5@gmail.com>
2024-12-10 11:52:53 +0100

    Reajustement du composant Input pour les champs de formulaire d'inscription; Configuration de useForm et passation des props register et errors aux input afin de recuperer les information envoye par l'utilisateur

--- src/pages/inscription/components/input.tsx
+++ src/pages/inscription/components/input.tsx
@@ -1,21 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {motion} from 'framer-motion'
 interface inputType {
     type: string,
     name: string,
-    placeholder: string
+    placeholder: string,
+    register: any,
+    errors: any
 }
 
 const inputVariant = {
+    initial: {
+        scale: 0.5,
+        y: -10,
+        opacity: 0,
+    },
+    animate: {
+        scale: 1,
+        y: 0,
+        opacity: 1,
+        transition: {
+            delay: 2
+        },
+    },
     hover: {
         y: -5
     }
 }
-const Input = ({type, name, placeholder}:inputType) => {
+const Input = ({type, name, placeholder,  register, errors }:inputType) => {
+    const errorMessage = `Le champs ${name} est requis`
     return (
-        <motion.input type={type} name={name} placeholder={placeholder} 
-        variants={inputVariant} whileHover="hover"
-        className="p-2 rounded-full bg-transparant border-4 border-black-marroon 
-        placeholder:text-black-marroon placeholder:text-sm text-black-marroon pl-4"
+        <motion.input 
+        type={type} 
+        {...register (name, {required: errorMessage})}
+        placeholder={placeholder} 
+        variants={inputVariant} initial="initial" animate="animate" whileHover="hover"
+        className={`p-2 rounded-full bg-transparant border-4  ${errors[name]? 'border-red' : 'border-black-marroon'}   
+        placeholder:text-black-marroon placeholder:text-sm text-black-marroon pl-4`}
         />
     );
 }
--- src/pages/inscription/step-1/step1.tsx
+++ src/pages/inscription/step-1/step1.tsx
@@ -3,6 +3,7 @@ import BtnSubmition from "../components/btnSubmition";
 import ConnexionLink from "../components/connexionLink";
 import Input from "../components/input";
 import {motion} from 'framer-motion'
+import { useForm } from "react-hook-form";
 
 const fieldsetVariant = {
     initial: {
@@ -44,17 +45,28 @@ const btnDivVariant = {
     }
 }
 
+interface Step1type {
+    name: string,
+    email: string,
+}
+
+console.clear()
+
 const Step1 = () => {
+    const {handleSubmit, register, formState: {errors}} = useForm<Step1type> ()
+    const onSubmit = (data: Step1type) => {console.log (data)}
     return (
         <main className='flex justify-center items-center p-8 w-screen bg-breaked-white h-screen'>
-            <form className='bg-white p-9 pl-8 pr-8 shadow-lg rounded-2xl flex flex-col sm:flex-row gap-5 items-center ' >
+            <form 
+                onSubmit={handleSubmit(onSubmit)}
+                className='bg-white p-9 pl-8 pr-8 shadow-lg rounded-2xl flex flex-col sm:flex-row gap-5 items-center ' >
                 <motion.img src="/img-step-1.jpg" className="~w-40/60" variants={imgVariant} initial="initial" animate="animate" />
                 <div className="flex flex-col gap-5 ">
                     <Header title="Créez un compte"/>
 
                     <motion.fieldset className="flex flex-col gap-3" variants={fieldsetVariant} initial="initial" animate="animate"  >
-                        <Input type="text" name="entrepiseName" placeholder="Nom de l'entreprise"/>
-                        <Input type="email" name="email" placeholder="E-mail"/>
+                        <Input type="text" register={register} errors={errors} name="entrepiseName" placeholder="Nom de l'entreprise"/>
+                        <Input type="email" register={register} errors={errors} name="email" placeholder="E-mail"/>
                     </motion.fieldset>
 
                     <motion.div className="flex flex-col items-center gap-2" variants={btnDivVariant} initial="initial" animate="animate" >
