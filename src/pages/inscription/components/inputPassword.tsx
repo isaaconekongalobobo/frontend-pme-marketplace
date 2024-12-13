76de66bccf5a2ecb2753fe88fb7e325bcf1b15a4
isaaconek <onekdev5@gmail.com>
2024-12-13 07:46:09 +0100

    Reajustement du champ password afin de bien alligner l'oeuil pour masquer ou afficher le mot de passe

--- src/pages/inscription/components/inputPassword.tsx
+++ src/pages/inscription/components/inputPassword.tsx
@@ -29,7 +29,7 @@ const InputPassword = ({name, register, errors }:inputType) => {
     const [inputType, setInputType] = useState (false)
     return (
         <motion.div 
-        className={`flex items-center text-black-marroon border-4 p-2 rounded-full ${errors[name]? 'border-red' : 'border-black-marroon'}
+        className={`flex items-center justify-between text-black-marroon border-4 p-2 rounded-full ${errors[name]? 'border-red' : 'border-black-marroon'}
             placeholder:text-black-marroon placeholder:text-sm text-black-marroon pl-4 `}
             variants={inputVariant} initial="initial" animate="animate" whileHover="hover"
         >
